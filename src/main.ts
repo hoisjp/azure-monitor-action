a4a85908d616807920d2f767ed58d0c759ae8beb
Issei Hiraoka <issei.hiraoka@gmail.com>
2021-07-08 17:33:46 +0900

    remove unnecessary code

--- src/main.ts
+++ src/main.ts
@@ -6,17 +6,9 @@ export async function run(): Promise<void> {
   try {
     // through secret
     const workspaceId: string = core.getInput('workspace-id', {required: true})
-    if (!workspaceId) {
-      throw new Error(
-        'Not all values are present in the creds object. Ensure clientId, clientSecret and tenantId are supplied.'
-      )
-    }
     const agentKey: string = core.getInput('agent-key', {required: true})
 
-    // let jsonBody: string = JSON.stringify(yaml.load(core.getInput('log', {required: true})))
-    const logMessage = core.getInput('log', {required: true})
-    const jsonBody = `${logMessage}`
-
+    const jsonBody = core.getInput('log', {required: true})
     core.debug(`input json: ${jsonBody}`)
 
     await azureMonitor.sendLogs(workspaceId, agentKey, jsonBody)
